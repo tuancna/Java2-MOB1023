2060567af0422958945b8d5701281a6238d2443e
Tuan Chung <tuanchung1997@gmail.com>
2021-05-24 09:06:45 +0700

    Add files via upload

---
+++ Lab 02/Bai4/UserList.java
@@ -0,0 +1,11 @@
+package Bai4;
+
+import java.util.ArrayList;
+
+public class UserList {
+    ArrayList<User> usl = new ArrayList();
+    
+    public void them(User us){
+        usl.add(us);
+    }
+}
